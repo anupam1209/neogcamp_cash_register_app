43bfd302ff136a330e7b8a9710c1336ec1c8c5dc
Anupam Prakash <anupamprakash82@gmail.com>
2022-09-15 15:19:39 +0530

    error msg handling and validation done

---
+++ index.js
@@ -0,0 +1,26 @@
+const billAmount = document.querySelector('#bill-amount');
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector('#check-button');
+const message = document.querySelector("#error-message"); 
+
+checkButton.addEventListener("click", function validateBillAmountandCashAmount(){
+    message.style.display = "none";
+    if(billAmount.value > 0){
+        if(cashGiven.value >= billAmount.value){
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+        }else{
+            showMessage("Are you out of your mind?")
+        }
+    }else{
+        showMessage("Invalid Bill Amount");
+    }
+} );
+
+function calculateChange(){
+
+}
+
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerText = msg;
+}
